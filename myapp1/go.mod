ee77bdd8af833e1ac7c81dbb9676b7511ea7e616
Charles Gallagher <cgallagher1@carthage.edu>
2025-11-23 00:51:34 -0600

    Added mysql database

--- myapp1/go.mod
+++ myapp1/go.mod
@@ -1,3 +1,7 @@
 module github.com/cgallagher1/myapp1
 
 go 1.25.4
+
+require github.com/go-sql-driver/mysql v1.9.3
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- myapp1/main.go
+++ myapp1/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, os.Getenv("MESSAGE"))
+		// MESSAGE from env
+		fmt.Fprintf(w, "Env MESSAGE: %s\n", os.Getenv("MESSAGE"))
+
+		// MESSAGE from MySQL
+		dbUser := os.Getenv("MYSQL_USER")
+		dbPass := os.Getenv("MYSQL_PASSWORD")
+		dbHost := os.Getenv("MYSQL_HOST") // set to 'mysql' in deployment
+		dbName := os.Getenv("MYSQL_DATABASE")
+
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", dbUser, dbPass, dbHost, dbName)
+		db, err := sql.Open("mysql", dsn)
+		if err != nil {
+			fmt.Fprintf(w, "DB error: %v", err)
+			return
+		}
+		defer db.Close()
+
+		var message string
+		appName := os.Getenv("APP_NAME")
+		err = db.QueryRow("SELECT message FROM messages WHERE app_name=?", appName).Scan(&message)
+		if err != nil {
+			fmt.Fprintf(w, "Query error: %v", err)
+			return
+		}
+		fmt.Fprintf(w, "MySQL MESSAGE: %s", message)
 	})
 
 	port := fmt.Sprintf(":%s", os.Getenv("LISTENANDSERVE"))
